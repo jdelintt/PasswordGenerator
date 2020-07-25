00c82034cc875a63c9ac91d5042821e017712d0f
Jesus DeLintt <jesus.delintt@gmail.com>
2020-07-24 23:00:23 -0700

    created objects and keys

--- Password.js
+++ Password.js
@@ -1,6 +1,40 @@
-var userChoice = prompt(
+var upperCaseCharacterArray = {
+  password: true,
+  array: ["P", "B", "V", "E", "M"],
+};
+var lowerCaseCharacterArray = {
+  password: true,
+  array: ["u", "n", "q", "r", "b"],
+};
+var specialCharacterArray = {
+  password: true,
+  array: ["#", "!", "*", "("],
+};
+var numberArray = {
+  password: true,
+  array: [7, 9, 2, 4, 5],
+};
+var prepArray = [];
+
+var userPassword = prompt(
   "Enter the number of characters you want in your password"
 );
-if (userChoice < 8 || userChoice > 128 || userChoice !== "") {
-  alert("Reload the page and try again");
+
+if (userPassword < 8 || userPassword > 128) {
+  alert("The number you typed in needs to be 8-128 characters. Try again!");
 }
+
+var userCharacterChoice = confirm("Do you want special characters?");
+var userNumberChoice = confirm("Do you want numbers?");
+var userLowerCaseChoice = confirm(
+  "Do you want the password to have lower case letters?"
+);
+var userUpperCaseChoice = confirm(
+  "Do you want the password to have upper case letters?"
+);
+
+var myCar = {
+  make: "Ford",
+  model: "Mustang",
+  year: 1969,
+};
